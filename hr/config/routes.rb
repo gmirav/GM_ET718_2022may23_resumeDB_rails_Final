cf00bf029d2b0a79a5ee8883bc1c2cd127a90139
EC2 Default User <ec2-user@ip-172-31-64-52.ec2.internal>
2022-05-23 14:38:38 +0000

    Pre-Refactoring

--- hr/config/routes.rb
+++ hr/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :resumes
-  get 'welcome/index'
+  resources :resumes do
+    resources :skills
+  end
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
